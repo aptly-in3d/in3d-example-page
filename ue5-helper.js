72eb8ea47cef84607c36707ad0880d3bf55c9be2
yoyoys <yoyoys@topctek.com>
2025-08-20 11:46:49 +0800

    init6

--- ue5-helper.js
+++ ue5-helper.js
@@ -1,27 +1,63 @@
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
-"object" != typeof ue && (ue = {}), uuidv4 = function () {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, function (t) {
-    return (t ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> t / 4).toString(16)
-  })
-},
-  ue5 = function (r) {
-    return "object" != typeof ue.interface || "function" != typeof
-      ue.interface.broadcast ? (ue.interface = {}, function (t, e, n, o) {
-        var u, i; "string" == typeof
-          t && ("function" == typeof e && (o = n, n = e, e = null), u = [t, "", r(n, o)], void 0 !== e && (u[1] = e),
-            i = encodeURIComponent(JSON.stringify(u)), "object" == typeof
-              history && "function" == typeof history.pushState ? (history.pushState({}, "", "#" + i),
-                history.pushState({}, "", "#" + encodeURIComponent("[]"))) : (document.location.hash = i,
-                  document.location.hash = encodeURIComponent("[]")))
-      }) : (i = ue.interface,
-        ue.interface = {}, function (t, e, n, o) {
-          var u; "string" == typeof t && ("function" == typeof
-            e && (o = n, n = e, e = null), u = r(n, o), void 0 !== e ? i.broadcast(t, JSON.stringify(e), u) :
-              i.broadcast(t, "", u))
-        });
-  }(function (t, e) {
-    if ("function" != typeof t) return ""; var
-      n = uuidv4(); return ue.ince[n] = t, setTimeout(function () { delete ue.interface[n] },
-        1e3 * Math.max(1, parseInt(e) || 0)), n
-  });
+
+// ESM module version of ue5-helper.js
+
+/**
+ * 產生 UUID v4 字串
+ */
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, t =>
+    (t ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> t / 4).toString(16)
+  );
+}
+
+// callback 註冊區（模組內部）
+const ince = {};
+
+function registerCallback(cb, timeoutSec) {
+  if (typeof cb !== 'function') return '';
+  const n = uuidv4();
+  ince[n] = cb;
+  setTimeout(() => { delete ince[n]; }, 1000 * Math.max(1, parseInt(timeoutSec) || 0));
+  return n;
+}
+
+function getUeInterface() {
+  if (typeof ue !== 'object') return null;
+  if (typeof ue.interface === 'object' && typeof ue.interface.broadcast === 'function') {
+    return ue.interface;
+  }
+  return null;
+}
+
+function ue5(event, payload, cb, timeoutSec) {
+  if (typeof event !== 'string') return;
+  if (typeof payload === 'function') {
+    timeoutSec = cb;
+    cb = payload;
+    payload = null;
+  }
+  const callbackId = registerCallback(cb, timeoutSec);
+  const i = getUeInterface();
+  if (i) {
+    // 有 broadcast
+    if (payload !== undefined) {
+      i.broadcast(event, JSON.stringify(payload), callbackId);
+    } else {
+      i.broadcast(event, '', callbackId);
+    }
+  } else {
+    // 無 broadcast，使用 hash
+    const arr = [event, '', callbackId];
+    if (payload !== undefined) arr[1] = payload;
+    const hashStr = encodeURIComponent(JSON.stringify(arr));
+    if (typeof history === 'object' && typeof history.pushState === 'function') {
+      history.pushState({}, '', '#' + hashStr);
+      history.pushState({}, '', '#' + encodeURIComponent('[]'));
+    } else {
+      document.location.hash = hashStr;
+      document.location.hash = encodeURIComponent('[]');
+    }
+  }
+}
